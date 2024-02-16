9d62e83f75109607a95795536b5070d7503ef74a
brij35h <softwarebrijesh@gmail.com>
2024-02-16 13:05:18 +0530

    update

--- src/Components/Settings.jsx
+++ src/Components/Settings.jsx
@@ -9,9 +9,9 @@ const Settings = () => {
         className="p-2 border border-slate-900 hover:border-slate-700 text-slate-200 justify-start flex items-center cursor-pointer hover:bg-slate-950 hover:shadow-xl  mt-3 w-[90%] mx-auto rounded "
       >
         <div className="px-3 me-3 rounded-full py-1 pb-3 block bg-red-500 flex items-center">
-          {navigator.userAgentData.brands[2].brand[0]}
+          {navigator.userAgentData?.brands[2].brand[0] || "Gemini"[0]}
         </div>
-        <span>{navigator.userAgentData.brands[2].brand}</span>
+        <span>{navigator.userAgentData?.brands[2].brand || "Gemini"}</span>
       </div>
     </div>
   );
